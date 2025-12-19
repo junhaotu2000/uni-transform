b11dff73b7cbfb6cf9b1ec50d2649d1878511873
junhao.tu <junhao.tu@astrabot.com>
2025-12-19 20:44:32 +0000

    clear up code

--- test.py
+++
@@ -1,9 +0,0 @@
-from uni_transform import Transform, transform_interpolate
-import torch
-import numpy as np
-
-tf1 = Transform.from_rep(torch.randn(1,9), from_rep="euler", extra_dims=4)
-tf2 = Transform.from_rep(torch.randn(1,9), from_rep="euler", extra_dims=3)
-
-print(tf1.to_rep("euler"))
-print(tf2.to_rep("euler"))
